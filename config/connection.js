9b967c5d68cbb93cd7d6c73e423628c8fe998eb7
Anastasiia Markova <anastasia19markova@gmail.com>
2023-03-09 15:03:31 -0500

    connection file

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,15 @@
 const { mongoose, connect, connection } = require('mongoose');
-// require("dotenv").config(); // To use environment variables.
+mongoose.set('strictQuery', true);
 
 // After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the application name and add your Atlas connection string as a Config Var
 // Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
-mongoose.connect(process.env.DB_NAME, { // Connection string to local instance of MongoDB including database name
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sn-api-usersDB';
+
+connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
+
+});
 
 module.exports = connection;
--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,5 +1,4 @@
 const { Schema, model, Types } = require('mongoose');
-const moment = require('moment');
 
 // Schema to create Thoughts model
 const reactionSchema = new Schema(
